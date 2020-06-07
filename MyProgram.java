96f049dc6fd1fe09159f90c44a1ef53e592f7228
nhowlade <nhowlade@uoguelph.ca>
2020-06-07 18:58:40 -0400

    Trying to draw vertical art of chess layout

--- MyProgram.java
+++ MyProgram.java
@@ -1,6 +1,23 @@
 public class MyProgram{
+    //These two functions can be used to makes vertical boxes
+
+    public static void drawBox()
+    {
+        System.out.println("  __");  //This art has two spaces at the front, 2 underscores and no following space
+        System.out.println("\n|    |"); //Between each bar we have 4 spaces
+    }
+    public static void oneLineBoxes()
+    {
+        int i = 0;
+        for(i=0; i<8; i++)
+        {
+            drawBox();
+        }
+    }
     public static void main(String[] args)
     {
-        System.out.println("Welcome to my Project 1");
+        System.out.println("Welcome to my Chess Game");
+        oneLineBoxes();
+        System.out.println("  __");
     }
 }
