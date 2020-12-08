bfa5b73c4deedb28249c309bd45014fd8bb12a76
nhowlade <nhowlade@uoguelph.ca>
2020-12-08 17:45:01 -0500

    Getting the pointer to pointer code

--- C-learning/pointer_basics.c
+++ C-learning/pointer_basics.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main(int argc, char **argv)
+#define LENGTH 3
+int data[LENGTH]
+
+    int
+    main(int argc, char **argv)
 {
     // int num;
     // int *numPtr;
@@ -9,9 +13,24 @@ int main(int argc, char **argv)
     // numPtr = &num;
     // num2 = *numPtr;
     // printf("num=%d, numPtr=%d, address of num=%d, num2=%d\n", num, numPtr, &num, num2);
-    char str1[] = "Hello World";
-    char *str2 = "Goodbye";
-    printf("%s %c %d %d %d\n", str1, str1[0], &str1, &str1[0], str1);
-    printf("%d %d %s\n", &str2, str2, str2);
+    // char str1[] = "Hello World";
+    // char *str2 = "Goodbye";
+    // printf("%s %c %d %d %d\n", str1, str1[0], &str1, &str1[0], str1);
+    // printf("%d %d %s\n", &str2, str2, str2);
+    int *pi;
+    int **ppi;
+
+    printf("multiple indirection example\n");
+
+    for (int i = 0; i < LENGTH; i++)
+    {
+        data[i] = i;
+    }
+
+    for (int i = 0; i < LENGTH; i++)
+    {
+        printf("%d\n", data[i]);
+    }
+
     return 0;
 }
