19a4e6f09b7eeaebed35b64e5665ad17c4193694
nhowlade <nhowlade@uoguelph.ca>
2020-12-11 16:24:54 -0500

    Making the malloc file

---
+++ C-learning/introMalloc.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+
+#define MAXSTRLEN 100
+
+char *string_function(char *aString)
+{
+    char *s;
+    s = (char *)malloc(MAXSTRLEN);
+    s[0] = 0;
+    strcat(s, "Hello");
+    strcat(s, aString);
+    strcat(s, "\n");
+    return s;
+}
+
+int main(int argc, char **argv)
+{
+    printf(string_function("FRED"));
+    printf(string_function("Gaussie Fink-Nottle"));
+}
