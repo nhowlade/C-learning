dabb2d32c06a4d553981bcec6d01fdc9993983c9
nhowlade <nhowlade@uoguelph.ca>
2020-12-11 15:54:55 -0500

    Manipulating integers with void pointer

--- C-learning/genericPointer.c
+++ C-learning/genericPointer.c
@@ -8,5 +8,29 @@ char *words[LENGTH];
 int main(int argc, char **argv)
 {
     void *gp;
+
+    for (int i = 0; i < LENGTH; i++)
+    {
+        data[i] = i;
+    }
+    for (int i = 0; i < LENGTH; i++)
+    {
+        printf("%d\n", data[i]);
+    }
+
+    words[0] = "one";
+    words[1] = "two";
+    words[2] = "three";
+
+    for (int i = 0; i < LENGTH; i++)
+    {
+        printf("%s\n", words[i]);
+    }
+
+    gp = data;
+    printf("\ndata array address is %p\n", gp);
+    printf("item pointed to by gp is %d\n", *(int *)gp);
+    gp = (int *)gp + 1;
+    printf("item pointed to by gp now is %d\n", *(int *)gp);
     return 0;
 }
