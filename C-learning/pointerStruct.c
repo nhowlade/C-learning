3b10a03b9bc50b08e4fa99f43fda02041f437551
nhowlade <nhowlade@uoguelph.ca>
2020-12-12 12:00:53 -0500

    Implemented the file till vide_25

--- C-learning/pointerStruct.c
+++ C-learning/pointerStruct.c
@@ -9,3 +9,23 @@ typedef struct
     double b;
     long long int d;
 } MYSTRUCT;
+
+#define COUNT 4
+
+int main()
+{
+    MYSTRUCT *p, *q;
+    void *v;
+    printf("The size of the struct is %d\n", sizeof(MYSTRUCT));
+    p = (MYSTRUCT *)calloc(COUNT, sizeof(MYSTRUCT));
+    for (int i = 0; i < COUNT; i++)
+    {
+        p[i].a = 1;
+        p[i].b = 1000000000.0 + i;
+        p[i].c = i * 20;
+        p[i].d = 42546 + i;
+    }
+
+    q = p;
+    q = p + 3;
+}
