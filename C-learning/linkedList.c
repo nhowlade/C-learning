a630f289bf4324be6ce3eb8ba81fcbe2f3b15f22
nhowlade <nhowlade@uoguelph.ca>
2020-12-15 23:11:40 -0500

    Populated the linked list

--- C-learning/linkedList.c
+++ C-learning/linkedList.c
@@ -6,3 +6,17 @@ typedef struct listitem
     struct listitem *next;
     int data;
 } LISTITEM;
+
+int main()
+{
+    LISTITEM *listhead, *temp;
+    listhead = NULL;
+
+    for (int i = 0; i < 3; i++)
+    {
+        temp = malloc(sizeof(LISTITEM));
+        temp->data = i;
+        temp->next = listhead;
+        listhead = temp;
+    }
+}
