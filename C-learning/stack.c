bef19b9c5c8d2e0f44f8ccccbea7c885a5ed5a3c
nhowlade <nhowlade@uoguelph.ca>
2020-12-16 15:46:32 -0500

    Made the stack demo

---
+++ C-learning/stack.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+typedef struct
+{
+    int a;
+    double b;
+} DATA;
+
+DATA *base;
+DATA *end;
+DATA *top;
+
+#define DATASIZE 5
+
+int main()
+{
+    return 0;
+}
