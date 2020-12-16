c2ebcacebc6f32ce79c2774a5eccf6f5d5f5d5f9
nhowlade <nhowlade@uoguelph.ca>
2020-12-15 23:39:09 -0500

    Initialised a doubly linked list

---
+++ C-learning/doublyList.c
@@ -0,0 +1,24 @@
+typedef struct listitem
+{
+    struct listitem *previous;
+    struct listitem *next;
+    int data;
+} LISTITEM;
+
+int main()
+{
+    LISTITEM *temp, head;
+    head.next = (LISTITEM *)&head;
+    head.previous = (LISTITEM *)&head;
+    head.data = -1;
+
+    for (int i = 0; i < 3; i++)
+    {
+        temp = malloc(sizeof(LISTITEM));
+        temp->data = i;
+        temp->next = head.next;
+        head.next = temp;
+        temp->previous = &head;
+        temp->next->previous = temp;
+    }
+}
