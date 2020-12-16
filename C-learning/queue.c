2b1a7273f8e597023bd9a7b43f694fecaf22193f
nhowlade <nhowlade@uoguelph.ca>
2020-12-16 12:08:14 -0500

    Understanding enqueue

--- C-learning/queue.c
+++ C-learning/queue.c
@@ -14,6 +14,17 @@ typedef struct listhead
     struct listhead *end;
 } LISTHEAD;
 
+LISTHEAD head;
+
+void enqueue(LISTITEM *item)
+{
+    LISTITEM *temp;
+    temp = head.end;                // Get the last item into a temp variable
+    head.end = item;                // Make the head end to point to the new element
+    item->previous = temp;          // Make the "old" item to be the previous link for the new item
+    item->next = (LISTHEAD *)&head; // Refer the new item to point to the head
+    temp->next = item;              //Make the "old" item to point to the new item
+}
 int main()
 {
     return 0;
