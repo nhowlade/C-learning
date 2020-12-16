cc58f78dede36ae886d8c3cd22fdbe0db7010861
nhowlade <nhowlade@uoguelph.ca>
2020-12-16 16:20:31 -0500

    Stack pop implemented

--- C-learning/stack.c
+++ C-learning/stack.c
@@ -13,6 +13,37 @@ DATA *top;
 
 #define DATASIZE 5
 
+int push(DATA *data)
+{
+    int ok;
+    if (top == end)
+    {
+        ok = 0;
+    }
+    else
+    {
+        memcpy(top, data, sizeof(DATA));
+        top++;
+        ok = 1;
+    }
+    return ok;
+}
+
+DATA *pop()
+{
+    DATA *data;
+    if (top == base)
+    {
+        data = NULL;
+    }
+    else
+    {
+        top = top - 1;
+        data = top;
+    }
+    return data;
+}
+
 int main()
 {
     return 0;
